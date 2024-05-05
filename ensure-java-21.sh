3c1ac91fb7f363f4b0e65e72b58c146d460286fd
Brycen G <brycengranville@outlook.com>
2024-05-05 03:10:51 -0400

    ci(jitpack): finish upgrading to java 21

--- ensure-java-21.sh
+++ ensure-java-21.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 
-JV=`java -version 2>&1 >/dev/null | head -1`
-echo $JV | sed -E 's/^.*version "([^".]*)\.[^"]*".*$/\1/'
+JV=$(java -version 2>&1 >/dev/null | head -1)
+echo "$JV" | sed -E 's/^.*version "([^".]*)\.[^"]*".*$/\1/'
 
-if [ "$JV" != 17 ]; then
+if [ "$JV" != 21 ]; then
 	case "$1" in
 	install)
 		echo "Installing SDKMAN..."
 		curl -s "https://get.sdkman.io" | bash
 		source ~/.sdkman/bin/sdkman-init.sh
 		sdk version
+		sdk env install
 		sdk env
 		;;
 	use)
